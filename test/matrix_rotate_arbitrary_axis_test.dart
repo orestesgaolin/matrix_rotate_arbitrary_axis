8236fdf13b5ccb765488f712de5838da1e2106a5
Dominik Roszkowski <dominik@roszkowski.dev>
2020-05-04 16:37:30 +0200

    Add implementation and test

---
+++ test/matrix_rotate_arbitrary_axis_test.dart
@@ -0,0 +1,33 @@
+// import 'dart:math' as math;
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:matrix_rotate_arbitrary_axis/matrix_rotate_arbitrary_axis.dart';
+
+void main() {
+  group('constructor', () {
+    // final pi = math.pi;
+    final a = 0.0;
+    final b = 0.0;
+    final c = 0.0;
+    // final u = 0.0;
+    final v = 0.0;
+    final w = 0.0;
+    final theta = 0.0;
+
+    test('should create identity matrix for u = 1', () {
+      final u1 = 1.0;
+      final rM = RotationMatrix(a, b, c, u1, v, w, theta);
+      final m = rM.getMatrix();
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+          final index = m.index(i, j);
+          if (j == i) {
+            equals(m.storage[index], 1);
+          } else {
+            equals(m.storage[index], 0);
+          }
+        }
+      }
+    });
+  });
+}
